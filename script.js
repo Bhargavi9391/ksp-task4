9f73a56e4017d194f119d53ee41f8cc1875defc6
Bhargavi9391 <157037629+Bhargavi9391@users.noreply.github.com>
2024-01-26 09:17:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+console.log("Carrex.club");
+
+window.alert("welcome to the page|click ok on the page");
+
+//using camel case
+
+valueOne=23;
+valueTwo=65;
+console.log(valueOne);
+console.log(valueTwo);
+
+//using let,const,var keywords 
+let bhar="bhargavi";
+console.log(bhar);
+
+const bhar2=34;
+bhar1=54;
+console.log(bhar2);
+
+var variable=67;
+variable=78;
+console.log(variable);
+
+
+//Airthemetic opertaors
+
+let value1=90;
+ value2=34;
+ console.log(value1+value2);
+ console.log(value1-value2);
+ console.log(value1*value2);
+ console.log(value1/value2);
+ console.log(value1++);
+ console.log(value2--);
+ console.log(value1**2);
+
+
+ //assignment operators
+
+ let a=2;
+ b=3;
+ console.log(a=b);
+ console.log(a**2);
+ console.log(a+8);
+
+ //comparison operator
+ let num=5;
+ res=num==5;
+ console.log(a==5);
+ console.log(a!=b);
+ console.log(a<b);
+ console.log(a>b);
+ console.log(a<b);
+ console.log(a<=b);
+ console.log(a>=b);
+ console.log(a=b);
+ console.log(res);
+
+ //datatypes
+ let n=23;
+ n2="bhar";
+ n3=43.8;
+ console.log(n==n2);
+ console.log(n2==n3);
+ console.log(n);
+ console.log(n2);
+ console.log(n3);
+
+ //logical operators
+ let num1=6;
+ num2=78;
+ console.log(num1<num2 && num2>num1);
+ console.log(num1==num2 || num2==num1);
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+ 
+
+
+
